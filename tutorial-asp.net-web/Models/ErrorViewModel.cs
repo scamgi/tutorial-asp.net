61934f4e361aee110a27d1c027a425495a9e9e82
scamgi <scampini.giacomo@gmail.com>
2024-03-19 00:29:29 +0100

    first commit

---
+++ tutorial-asp.net-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace tutorial_asp.net_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
